9e6e20327b69a29bd71d31c6fb8dbdd1f36028fb
Angel Centeno <angel.centeno@evokad.com>
2018-07-22 22:56:45 -0400

    function

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 // Enqueue Styles in Header
 function mydmk_css() {
+	wp_enqueue_script( 'jq-js', get_home_url().'/wp-content/themes/mydmk/jquery/jquery-3.3.1.min.js');
   wp_enqueue_style( 'mydmk-style-main', get_home_url() .'/wp-content/themes/mydmk/style.css' );
 	wp_enqueue_style( 'mydmk-style-custom', get_home_url() .'/wp-content/themes/mydmk/dist/css/custom.css' );
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -34,10 +35,12 @@ add_action( 'wp_enqueue_scripts', 'mydmk_css' );
 
 // Enqueue Scripts in Footer
 function mydmk_js() {
+	//wp_enqueue_script( 'jq-js', get_home_url().'/wp-content/themes/mydmk/jquery/jquery-3.3.1.min.js');
 	wp_enqueue_script( 'main-js', get_home_url().'/wp-content/themes/mydmk/dist/js/all.js');
 	wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/e66dca7e8d.js' );
 }
 add_action( 'get_footer', 'mydmk_js' );
+
 
 // Remove excess WP default items
 remove_action( 'wp_head', 'wp_generator' );
